973b258b2101a4b78a67a3ac024350e1b2ba517d
seoyusang <vlvksbdof123@naver.com>
2023-11-01 17:28:30 +0900

    change main

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 const lib = require("./lib");
 
 if (process.argv.length <= 3) {
@@ -10,24 +9,27 @@ if (process.argv.length <= 3) {
 let command = process.argv[2];
 
 let numbers = process.argv
-    .slice(3, process.argv.length)
-    .map((n) => parseFloat(n));
+.slice(3, process.argv.length)
+.map((n)=>parseFloat(n));
 
-if (numbers.some((n) => isNaN(n))) {
+if (numbers.some((n)=>isNaN(n))) {
     console.log("Some arguments are not numbers!");
     process.exit(1);
 }
 
 let result;
-switch (command) {
+switch(command){
+    // 평균 구현
     case "avg":
         result = lib.avg(numbers);
         break;
+        // 소수 구현
     case "prime":
         result = lib.prime(numbers[0]);
         break;
+        // 팩토리얼 구현
     case "fact":
-        result = lib.fact(numbers[0])
+        result = lib.fact(numbers[0]);
         break;
     default:
         console.log("Wrong command!");
@@ -35,3 +37,7 @@ switch (command) {
 }
 
 console.log(result);
+
+// export.avg = avg;
+// export.prime = prime;
+// export fact = fact;
